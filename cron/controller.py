5b57223830df279dba19953ede6536859ce0341e
twa127 <46624596+twa127@users.noreply.github.com>
2023-02-20 16:35:42 +0000

    Add files via upload

--- cron/controller.py
+++ cron/controller.py
@@ -992,12 +992,12 @@ try:
                         boost_active = 1
                         if dbgLevel >= 2:
                             print(bc.dtm + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + bc.ENDC + " - Boost is Active for This Zone")
-                    elif datetime.datetime.now() >= boost_time + datetime.timedelta(minutes = boost_minute) and  boost_status == 1:
+                    elif (timestamp - boost_time).total_seconds() >= (boost_minute * 60) and  boost_status == 1:
                         boost_active = 0
                         #You can comment out if you dont have Boost Button Console installed.
                         cur.execute(
                             "SELECT * FROM boost WHERE zone_id = %s AND status = '1';",
-                            (zone_id, ),
+                            (m[messages_out_to_index["id"]], ),
                         )
                         if cur.rowcount > 0:
                             brow = cur.fetchone()
@@ -1006,13 +1006,13 @@ try:
                             boost_button_child_id = brow[brow_to_index['boost_button_child_id']]
                             cur.execute(
                                 "UPDATE messages_out SET payload = %s, sent = '0' WHERE zone_id = %s AND node_id = %s AND child_id = %s LIMIT 1;",
-                                [str(boost_active), zone_id, boost_button_id, boost_button_child_id],
+                                [str(boost_active), m[messages_out_to_index["id"]], boost_button_id, boost_button_child_id],
                             )
                             con.commit()  # commit above
                             #update Boost Records in database
                             cur.execute(
                                 "UPDATE boost SET status = %s, sync = '0' WHERE zone_id = %s AND status = '1';",
-                                [str(boost_active), zone_id],
+                                [str(boost_active), m[messages_out_to_index["id"]]],
                             )
                             con.commit()  # commit above
                     else:
@@ -2641,8 +2641,8 @@ try:
                             node_to_index = dict((d[0], i) for i, d in enumerate(cur.description))
                             node_id = node[node_to_index["node_id"]]
                             cur.execute(
-                                "UPDATE messages_out SET sent = '0', payload = '%s' WHERE node_id = %s AND child_id = %s LIMIT 1;",
-                                [new_system_controller_status, node_id, on_relay_child_id],
+                                "UPDATE messages_out SET sent = '0', payload = '0' WHERE node_id = %s AND child_id = %s LIMIT 1;",
+                                [node_id, on_relay_child_id],
                             )
                             con.commit()  # commit above
                             if dbgLevel >= 2:
@@ -2657,8 +2657,8 @@ try:
                         node_to_index = dict((d[0], i) for i, d in enumerate(cur.description))
                         node_id = node[node_to_index["node_id"]]
                         cur.execute(
-                            "UPDATE messages_out SET sent = '0', payload = '%s' WHERE node_id = %s AND child_id = %s LIMIT 1;",
-                            [new_system_controller_status, node_id, fan_relay_child_id],
+                            "UPDATE messages_out SET sent = '0', payload = '0' WHERE node_id = %s AND child_id = %s LIMIT 1;",
+                            [node_id, fan_relay_child_id],
                         )
                         con.commit()  # commit above
                         if dbgLevel >= 2:
@@ -2853,12 +2853,12 @@ try:
                             if active_sc_mode == 5: #HVAC fan ON if set to fan mode, else turn OFF
                                 cur.execute(
                                     "UPDATE messages_out SET sent = 0, payload = '1' WHERE node_id = %s AND child_id = %s LIMIT 1;",
-                                    [node_id, fan_relay_child_id],
+                                    [node_id, cool_relay_child_id],
                                 )
                             else:
                                 cur.execute(
                                     "UPDATE messages_out SET sent = '0', payload = '%s' WHERE node_id = %s AND child_id = %s LIMIT 1;",
-                                    [new_system_controller_status, node_id, fan_relay_child_id],
+                                    [new_system_controller_status, node_id, heat_relay_child_id],
                             )
                             con.commit()  # commit above
                             if dbgLevel >= 2:
