f6cda16bf1b9ed11ea6a9592338ff6323a119b28
twa127 <46624596+twa127@users.noreply.github.com>
2023-03-15 15:34:27 +0000

    Update gateway.py

--- cron/gateway.py
+++ cron/gateway.py
@@ -497,11 +497,11 @@ def on_message(client, userdata, message):
             correction_factor = float(result[sensor_to_index["correction_factor"]])
             mqtt_payload = mqtt_payload + correction_factor
             # Update last reading for this sensor
-            cur.execute(
-                "UPDATE `sensors` SET `current_val_1` = %s WHERE sensor_id = %s AND sensor_child_id = %s",
-                [mqtt_payload, sensor_id, mqtt_child_sensor_id],
+            cur_mqtt.execute(
+                "UPDATE `sensors` SET `current_val_1` = %s WHERE sensor_id = %s AND sensor_child_id = %s;",
+                [mqtt_payload, sensor_id, ,mqtt_child_sensor_id],
             )
-            con.commit()
+            con_mqtt.commit()
             if mode == 1:
                 # Get previous data for this sensorr
                 cur_mqtt.execute(
